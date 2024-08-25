84509bd09f1ae36698469cf73c87665774f46375
oriash93 <oriash93@gmail.com>
2024-08-25 16:39:16 +0300

    WIP
    # Conflicts:
    #	AutoClicker/App.xaml.cs

--- AutoClicker/App.xaml.cs
+++ AutoClicker/App.xaml.cs
@@ -1,11 +1,52 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace AutoClicker
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            SetupUnhandledExceptionHandling();
+        }
+
+        private void SetupUnhandledExceptionHandling()
+        {
+            // Catch exceptions from all threads in the AppDomain.
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+                ShowUnhandledException(args.ExceptionObject as Exception, "AppDomain.CurrentDomain.UnhandledException", false);
+
+            // Catch exceptions from a single specific UI dispatcher thread.
+            Dispatcher.UnhandledException += (sender, args) =>
+            {
+                // If we are debugging, let Visual Studio handle the exception and take us to the code that threw it.
+                if (!Debugger.IsAttached)
+                {
+                    args.Handled = true;
+                    ShowUnhandledException(args.Exception, "Dispatcher.UnhandledException", true);
+                }
+            };
+        }
+
+        void ShowUnhandledException(Exception e, string unhandledExceptionType, bool promptUserForShutdown)
+        {
+            var messageBoxTitle = $"Unexpected Error Occurred: {unhandledExceptionType}";
+            var messageBoxMessage = $"The following exception occurred:\n\n{e}";
+            var messageBoxButtons = MessageBoxButton.OK;
+
+            if (promptUserForShutdown)
+            {
+                messageBoxMessage += "\n\nNormally the app would die now. Should we let it die?";
+                messageBoxButtons = MessageBoxButton.YesNo;
+            }
+
+            // Let the user decide if the app should die or not (if applicable).
+            if (MessageBox.Show(messageBoxMessage, messageBoxTitle, messageBoxButtons) == MessageBoxResult.Yes)
+            {
+                Current.Shutdown();
+            }
+        }
     }
 }
